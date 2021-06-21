ffc54f3b16fa793d16463a41390becfe7556f867
ArcadeHustle <hostile@me.com>
2021-06-21 13:27:09 -0400

    add all banned books

--- DrSeussBeta.sh
+++ DrSeussBeta.sh
@@ -1,2 +1,32 @@
 #!/bin/sh
-/media/fat/jpegtofb -s 5 /media/fat/Mulberry/*
+PS3='Want to read a banned Dr Seuss book? '
+books=("CatsQuizzer" "McElligots" "Mulberry" "OnBeyond", "RanTheZoo", "ScrambledEggs", Quit")
+
+select banned in "${books[@]}"; do
+    case $banned in
+        "CatsQuizzer")
+	    /media/fat/jpegtofb -s 5 /media/fat/CatsQuizzer/*.jpeg
+            ;;
+        "McElligots")
+	    /media/fat/jpegtofb -s 5 /media/fat/McElligots/*.jpeg
+            ;;
+        "Mulberry")
+	    /media/fat/jpegtofb -s 5 /media/fat/Mulberry/*.jpeg
+            ;;
+        "OnBeyond")
+	    /media/fat/jpegtofb -s 5 /media/fat/OnBeyond/*.jpeg
+            ;;
+        "RanTheZoo")
+	    /media/fat/jpegtofb -s 5 /media/fat/RanTheZoo/*.jpeg
+            ;;
+        "ScrambledEggs")
+	    /media/fat/jpegtofb -s 5 /media/fat/ScrambledEggs/*.jpeg
+            ;;
+	"Quit")
+	    echo "User requested exit"
+	    exit
+	    ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
---
+++ install.sh
@@ -0,0 +1,4 @@
+cp jpegtofb /media/fat
+cp -rf CatsQuizzer McElligots Mulberry OnBeyond RanTheZoo ScrambledEggs /media/fat
+cp DrSeussBeta.sh /media/fat/Scripts
+
