6b837797d9946a0d82f5105ac5951c7c95543bd3
ArcadeHustle <hostile@me.com>
2021-06-21 15:35:03 -0400

    fix delays

--- DrSeussBeta.sh
+++ DrSeussBeta.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 PS3='Want to read a banned Dr Seuss book? '
-books=("CatsQuizzer" "McElligots" "Mulberry" "OnBeyond", "RanTheZoo", "ScrambledEggs", Quit")
+books=("CatsQuizzer" "McElligots" "Mulberry" "OnBeyond", "RanTheZoo", "ScrambledEggs", "Quit")
 
 select banned in "${books[@]}"; do
     case $banned in
         "CatsQuizzer")
-	    /media/fat/jpegtofb -s 5 /media/fat/CatsQuizzer/*.jpeg
+	    fbv -efi -d 30  /media/fat/CatsQuizzer/*.jpeg
             ;;
         "McElligots")
-	    /media/fat/jpegtofb -s 5 /media/fat/McElligots/*.jpeg
+	    fbv -efi -d 30  /media/fat/McElligots/*.jpeg
             ;;
         "Mulberry")
-	    /media/fat/jpegtofb -s 5 /media/fat/Mulberry/*.jpeg
+	    fbv -efi -d 30  /media/fat/Mulberry/*.jpeg
             ;;
         "OnBeyond")
-	    /media/fat/jpegtofb -s 5 /media/fat/OnBeyond/*.jpeg
+	    fbv -efi -d 30  /media/fat/OnBeyond/*.jpeg
             ;;
         "RanTheZoo")
-	    /media/fat/jpegtofb -s 5 /media/fat/RanTheZoo/*.jpeg
+	    fbv -efi -d 30  /media/fat/RanTheZoo/*.jpeg
             ;;
         "ScrambledEggs")
-	    /media/fat/jpegtofb -s 5 /media/fat/ScrambledEggs/*.jpeg
+	    fbv -efi -d 30  /media/fat/ScrambledEggs/*.jpeg
             ;;
 	"Quit")
 	    echo "User requested exit"
