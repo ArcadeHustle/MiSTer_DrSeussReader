3a4df0659608693d14bc894bcfb3d407926cbf46
ArcadeHustle <hostile@me.com>
2021-06-21 15:58:35 -0400

    fix books

--- DrSeussBeta.sh
+++ DrSeussBeta.sh
@@ -1,32 +1,33 @@
 #!/bin/sh
 PS3='Want to read a banned Dr Seuss book? Press enter for book list: '
-books=("CatsQuizzer" "McElligots" "Mulberry" "OnBeyond", "RanTheZoo", "ScrambledEggs", "Quit")
+books=("CatsQuizzer" "McElligots" "Mulberry" "OnBeyond" "RanTheZoo" "ScrambledEggs" "Quit")
 
 select banned in "${books[@]}"; do
     case $banned in
         "CatsQuizzer")
-	    fbv -efi -d 30  /media/fat/CatsQuizzer/*.jpeg
+	    fbv -efi -d 30 /media/fat/CatsQuizzer/*.jpeg
             ;;
         "McElligots")
-	    fbv -efi -d 30  /media/fat/McElligots/*.jpeg
+	    fbv -efi -d 30 /media/fat/McElligots/*.jpeg
             ;;
         "Mulberry")
-	    fbv -efi -d 30  /media/fat/Mulberry/*.jpeg
+	    fbv -efi -d 30 /media/fat/Mulberry/*.jpeg
             ;;
         "OnBeyond")
-	    fbv -efi -d 30  /media/fat/OnBeyond/*.jpeg
+	    fbv -efi -d 30 /media/fat/OnBeyond/*.jpeg
             ;;
         "RanTheZoo")
-	    fbv -efi -d 30  /media/fat/RanTheZoo/*.jpeg
+	    fbv -efi -d 30 /media/fat/RanTheZoo/*.jpeg
             ;;
         "ScrambledEggs")
-	    fbv -efi -d 30  /media/fat/ScrambledEggs/*.jpeg
+	    fbv -efi -d 30 /media/fat/ScrambledEggs/*.jpeg
             ;;
 	"Quit")
 	    echo "User requested exit"
 	    exit
 	    ;;
-        *) echo "invalid option $REPLY";;
+        *) echo "invalid option $REPLY"
+            ;;
     esac
 done
 
